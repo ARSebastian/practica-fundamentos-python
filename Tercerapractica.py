02028b9418b0a2e55a8c8bf8e032dc4d4c8bb204
Jesús Sebastián Aguilar Roque <vce08803@innovaccion.mx>
2021-08-24 19:05:22 -0500

    first commit

--- Tercerapractica.py
+++ Tercerapractica.py
@@ -9,7 +9,7 @@ print(random.randrange(10, 100, 2))
 
 #Declara la lista
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#imprime la lista
+#Imprime la lista
 print("lista original", lista)
 #mezcla los elementos de la lista al azar
 random.shuffle(lista)
